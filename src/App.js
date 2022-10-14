a321993733f640592736c375ccfe1bdb16b5a23d
Michael Smith <123craftftw@gmail.com>
2022-10-14 13:18:04 -0400

    reset

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import "./App.css";
 import React from "react";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Home from "./components/pages/Home";
-import Login from "./components/pages/Login";
-import Register from "./components/pages/Register";
-import Dashboard from "./components/pages/Dashboard";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Router>
-        <Navbar />
         <Routes>
-          <Route path='/' index element={<Home />} />
-          <Route path='/register'  element={<Register />} />
-          <Route path='/login'  element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          
         </Routes>
-      </Router>
-      <Router>
-        <Footer />
       </Router>
     </>
   );
